58f59cdf92261ce2bd5ec8fdba76aca2e596f77b
inDlabs <indlabs@gmail.com>
2017-04-22 14:25:06 -0400

    basic tcp client/server

--- tcp_client.py
+++ tcp_client.py
@@ -1,7 +1,7 @@
 import socket
 
-target_host = "www.google.com"
-target_port = 80
+target_host = "0.0.0.0"
+target_port = 9999
 
 #sock object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,7 +10,7 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
 
 #send data
-client.send("GET / HTTP/1.1\r\nHost: google.com/r/n/r/n")
+client.send("ABCDEF")
 
 #receive data
 response = client.recv(4096)
